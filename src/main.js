69e3fec7e9c6c9040e791108dd2c2a2398f88ce0
Rachel Hall <rachelh159@gmail.com>
2022-07-31 15:22:04 -0600

    Using the then() function, writing code

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 const promise = welcome();
 
 console.log(promise);
+
+const question = "Will the weather be nice today?";
+const tellPromise = tell(question);
+// use a then() function to log the fortune's response 
+tellPromise.then((fortune) => {
+    console.log(question);
+    console.log(fortune);
+});
+
+// use external variables!
+// remember that you can use variables outside of the scope
+// of the then() callback function
+
+
+
+
